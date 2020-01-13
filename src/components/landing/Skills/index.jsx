3b82dc9645e34486ba56de29c40375d4952daeb5
Goldpm <lowedavidadam@gmail.com>
2020-01-13 02:16:43 -0500

    edited skills component

--- src/components/landing/Skills/index.jsx
+++ src/components/landing/Skills/index.jsx
@@ -1,24 +1,32 @@
 import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/skills.svg';
+import collage from 'assets/images/collage.png';
 import { Wrapper, SkillsWrapper, Details, Thumbnail } from './styles';
+
+const skillArr = ['HTML5, CSS3 and JavaScript', 'jQuery and Bootstrap', 'REACT', 'Node.js', 'MongoDB and MySQL'];
+
+const skillRet = skillArr.map(function(skill) {
+  return (
+    <li>
+      <p>{skill}</p>
+    </li>
+  );
+});
 
 export const Skills = () => (
   <Wrapper id="about">
     <SkillsWrapper as={Container}>
       <Thumbnail>
-        <img src={dev} alt="I'm Adam, and I'm a Full-Stack Web Developer!" />
+        <img src={collage} alt="I'm Adam, and I'm a Full-Stack Web Developer!" />
       </Thumbnail>
       <Details>
-        <h1>Hi There!</h1>
+        <h1>A Little about me...</h1>
         <p>
-          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry’s
-          standard dummy.
+          I am very passionate about coding, and recently graduated from UNCC's intensive Full-Stack bootcamp. Through
+          this I've become very familiar with-
         </p>
-        <Button as={AnchorLink} href="#contact">
-          Hire me
-        </Button>
+        <ul>{skillRet}</ul>
       </Details>
     </SkillsWrapper>
   </Wrapper>
