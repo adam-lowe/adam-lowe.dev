4bd0a1c2ae0ee23697bc102d2fd178d654afb8a7
Goldpm <lowedavidadam@gmail.com>
2020-01-13 02:16:16 -0500

    edited intro component

--- src/components/landing/Intro/index.jsx
+++ src/components/landing/Intro/index.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import AnchorLink from 'react-anchor-link-smooth-scroll';
 import { Header } from 'components/theme';
 import { Container, Button } from 'components/common';
-import dev from 'assets/illustrations/dev.svg';
+import me from 'assets/images/me.jpg';
 import { Wrapper, IntroWrapper, Details, Thumbnail } from './styles';
 
 export const Intro = () => (
@@ -11,13 +11,11 @@ export const Intro = () => (
     <IntroWrapper as={Container}>
       <Details>
         <h1>Hi There!</h1>
-        <h4>I'm Adam, and I'm a Full-Stack Web Developer!</h4>
-        <Button as={AnchorLink} href="#contact">
-          Hire me
-        </Button>
+        <h4>My name is Adam Lowe, and I'm a Full-Stack Web Developer!</h4>
+        <h6>(please excuse the mess, this site is brand new!)</h6>
       </Details>
       <Thumbnail>
-        <img src={dev} alt="I'm Adam, and I'm a Full-Stack Web Developer!" />
+        <img src={me} width="5%" height="5%" alt="I'm Adam, and I'm a Full-Stack Web Developer!" />
       </Thumbnail>
     </IntroWrapper>
   </Wrapper>
