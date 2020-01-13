00d0102515e6cc12048a693fd2311bdb68083fb5
Goldpm <lowedavidadam@gmail.com>
2020-01-13 00:24:20 -0500

    Replaced Title with logo

--- src/components/theme/Header/Navbar/index.jsx
+++ src/components/theme/Header/Navbar/index.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import logo from 'assets/images/logo.png';
 import { Container } from 'components/common';
 import NavbarLinks from '../NavbarLinks';
 import { Wrapper } from './styles';
 
 const Navbar = () => (
   <Wrapper as={Container}>
-    <Link to="/">Adam Lowe</Link>
+    <Link to="/">
+      <img src={logo} width="50px" height="50px" alt="It's my logo!" />
+    </Link>
     <NavbarLinks desktop />
   </Wrapper>
 );
