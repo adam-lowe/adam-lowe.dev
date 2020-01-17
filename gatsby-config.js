0dd58846e71e88798eb3f2d767f22b9e9cba9465
Goldpm <lowedavidadam@gmail.com>
2020-01-17 00:42:18 -0500

    changed icon name

--- gatsby-config.js
+++ gatsby-config.js
@@ -50,7 +50,7 @@ module.exports = {
         background_color: config.backgroundColor,
         theme_color: config.themeColor,
         display: 'minimal-ui',
-        icon: './static/favicon/favicon-512.png',
+        icon: './static/favicon/adam-logo.png',
       },
     },
     'gatsby-plugin-offline',
